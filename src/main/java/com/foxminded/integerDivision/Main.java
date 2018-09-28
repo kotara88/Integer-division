67536350a65e7903fc34148b18949e7e028b7eb3
mikhail <mkotara8827@gmail.com>
2018-09-29 00:10:35 +0300

    some improvements

--- src/main/java/com/foxminded/integerDivision/Main.java
+++ src/main/java/com/foxminded/integerDivision/Main.java
@@ -7,23 +7,21 @@ public class Main {
     public static void main(String[] args) {
         boolean flag = true;
         Main mainClass = new Main();
-
         while (flag) {
             int dividend = mainClass.inputNumber("dividend");
             int divisor = mainClass.inputNumber("divisor");
-
             if (divisor == 0) {
                 System.out.println("Divisor can't be zero!!");
                 continue;
             }
+            System.out.println("Result: ");
             if (Math.abs(dividend) < divisor){
-                System.out.println(dividend + "/" + divisor + " = " + dividend / divisor);
-                break;
+                System.out.println(dividend + "/" + divisor + " = " + dividend / divisor + "\n");
+                continue;
             }
-
             Divider divider = new Divider();
             String result = divider.doDivision(dividend, divisor);
-            System.out.println(result);
+            System.out.println(result + "\n");
         }
     }
 
@@ -31,10 +29,8 @@ public class Main {
         System.out.println("Input a " + numberName);
         Scanner scanner = new Scanner(System.in);
         String numberStr = scanner.nextLine();
-
         if (numberStr.equals("q"))
             System.exit(0);
-
         int dividend;
         try {
             dividend = Integer.parseInt(numberStr);
@@ -45,5 +41,4 @@ public class Main {
         }
         return dividend;
     }
-
 }
